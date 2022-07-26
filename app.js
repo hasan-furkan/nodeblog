22f4d5059f0a1f6422af7b721143e4a960caf14c
hasan-furkan <81443587+hasan-furkan@users.noreply.github.com>
2022-07-26 15:20:59 +0300

    commit

--- app.js
+++ app.js
@@ -1,43 +1,105 @@
-const http = require("http");
-const fs = require("fs")
-
+const express = require("express")
+const app = express()
 const hostName = "127.0.0.1";
-const port = 5000;
+const port = 5000
 
-const indexPage = fs.readFileSync("index.html")
-const aboutPage = fs.readFileSync("about.html")
-const contactPage = fs.readFileSync("contact.html")
-const notFoundPage = fs.readFileSync("404.html")
+const path = require("path")
+
+app.use(express.static("public"))
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "site/index.html"))
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "site/about.html"))
+})
+
+app.get("/blog", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "site/blog.html"))
+})
+
+app.listen(port, hostName, () => console.log(`server calisiyor http://${hostName}:${port}/`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require("http");
+
+
+// const hostName = "127.0.0.1";
+// const port = 5000;
+
+// const fs = require("fs")
+
+// const indexPage = fs.readFileSync("index.html")
+// const aboutPage = fs.readFileSync("about.html")
+// const contactPage = fs.readFileSync("contact.html")
+// const notFoundPage = fs.readFileSync("404.html")
+
+// // const server = http.createServer((req, res) => {
+// //     if(req.url === "/"){
+// //         return res.end("Home Page")
+// //     }else if(req.url === "/about"){
+// //         return res.end("About Page")
+// //     }else if(req.url === "/contact"){
+// //         return res.end("Contact Page")
+// //     }else {
+// //         res.statusCode = 404
+// //         res.end("SAYFA BULUNAMADi")
+// //     }
+    
+// // });
 
 // const server = http.createServer((req, res) => {
 //     if(req.url === "/"){
-//         return res.end("Home Page")
+//         return res.end(indexPage)
 //     }else if(req.url === "/about"){
-//         return res.end("About Page")
+//         return res.end(aboutPage)
 //     }else if(req.url === "/contact"){
-//         return res.end("Contact Page")
+//         return res.end(contactPage)
 //     }else {
 //         res.statusCode = 404
-//         res.end("SAYFA BULUNAMADi")
+//         res.end(notFoundPage)
 //     }
     
 // });
 
-const server = http.createServer((req, res) => {
-    if(req.url === "/"){
-        return res.end(indexPage)
-    }else if(req.url === "/about"){
-        return res.end(aboutPage)
-    }else if(req.url === "/contact"){
-        return res.end(contactPage)
-    }else {
-        res.statusCode = 404
-        res.end(notFoundPage)
-    }
+
+// server.listen(port, hostName, ()=> {
+//     console.log(`server calisiyor http://${hostName}:${port}/`);
+// } )
+
+
+// app.use("/test", (req,res,next)=> {
+    //     console.log("Ben middleware olarak calisiyorum");
+    //     next()
+    // })
     
-});
-
-
-server.listen(port, hostName, ()=> {
-    console.log(`server calisiyor http://${hostName}:${port}/`);
-} )
+    // app.get("/", (req,res)=> {
+    //     res.sendFile(path.resolve(__dirname, "index.html"))
+    // })
+    
+    // app.get("/about", (req,res)=> {
+    //     res.sendFile(path.resolve(__dirname, "about.html"))
+    // })
+    
+    // app.get("/contact", (req,res)=> {
+    //     res.sendFile(path.resolve(__dirname, "contact.html"))
+    // })
+    
